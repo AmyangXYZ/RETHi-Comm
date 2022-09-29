f677898bdfbdf9ab5c37dc2790497c5bb8c2e42e
AmyangXYZ <amyang.xyz@gmail.com>
2022-09-28 20:34:50 -0400

    adding schedule table

--- frontend/src/components/Schedule.vue
+++ frontend/src/components/Schedule.vue
@@ -18,6 +18,8 @@ import "echarts/lib/component/legend";
 import "echarts/lib/component/dataZoom";
 import "echarts/lib/component/tooltip";
 
+const scheduleL2 = require("../assets/schedule_level2.json");
+
 export default {
   components: {
     ECharts,
@@ -29,7 +31,7 @@ export default {
         grid: {
           top:"13%",
           bottom:"2%",
-          left:"3%",
+          left:"4%",
           right:"2%",
         },
         xAxis: {
@@ -47,14 +49,18 @@ export default {
           },
           axisLabel: {
             fontSize:13,
+            formatter:(item)=>{
+              return item*800
+            }
           },
-          data:[1,2,3]
+          data:[],
+
         },
         yAxis: {
           type: "category",
           name:"Ports",
           nameLocation:"center",
-          nameGap: 25,
+          nameGap: 45,
           nameTextStyle: {
             fontSize: 14,
             fontWeight:"600"
@@ -63,12 +69,12 @@ export default {
             show: true
           },
           axisLabel: {
-            fontSize:13,
+            fontSize:14,
           },
-          data:[1,2,3]
+          data:[]
         },
         visualMap: {
-          type:"piecewise",
+          // type:"piecewise",
           show:false,
           pieces: [],
           min: 0,
@@ -91,15 +97,47 @@ export default {
                 shadowColor: 'rgba(0, 0, 0, 0.5)'
               }
             },
-            data: [[1,1,1], [2,2,2]]
+            data: [],
+            animation:false
           }
         ]
+      }
+    }
+  },
+  methods: {
+    draw() {
+      this.option.xAxis.data = []
+      this.option.yAxis.data = []
+      this.option.series[0].data = []
+
+      for (var i=0;i<=5000000/800;i++) {
+        this.option.xAxis.data.push(i)
+      }
+
+      for (var link in scheduleL2) {
+        var sw = link.match(/\d+/g)[0]
+        if (this.selectedNode.match(/\d+/g)[0]==sw) {
+          var port = `to ${link.match(/\d+/g)[1]}`
+          this.option.yAxis.data.push(port)
+          var windows = scheduleL2[link]
+          for (var j=0;j<windows.length;j++) {
+            var cell = windows[j]
+            for (var t=cell[1]/800;t<cell[2]/800;t++) {
+              this.option.series[0].data.push({
+                value: [t,port,cell[0]+2],
+                itemStyle: {},
+                label: {}
+              })
+            }
+          }
+        }
       }
     }
   },
   mounted() {
     this.$EventBus.$on("selectedNode", (node)=>{
       this.selectedNode = node
+      this.draw()
     })
   }
 }
